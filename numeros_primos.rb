34e132853da4a62a767a9f5dda17512344f468a9
codeamac02 <codeamac02@gmail.com>
2015-06-03 20:57:01 -0500

    Numeros Primos + factor

--- numeros_primos.rb
+++ numeros_primos.rb
@@ -1,4 +1,4 @@
-def identify_prime_factors(integer)
+def identify_prime_factors?(integer)
 	c = 0
 	for i in (1..integer)
 		if integer % i == 0
@@ -6,23 +6,44 @@ def identify_prime_factors(integer)
 		end
 	c
 	end
-	
-	if c == 2
-		integer
-	end
+	c == 2
 end
-identify_prime_factors(18)
+# p identify_prime_factors?(19)
 
 def numeros_primos(integer)
 	my_aray2 = []
-	for i in (2..10)
-		a = identify_prime_factors(i)
-		my_aray2.push(a)
+	for i in (2..integer)
+		a = identify_prime_factors?(i)
+		if a == true
+			my_aray2.push(i)
+		end
 	end
-	p my_aray2
+	my_aray2
+end
+# p numeros_primos(9)
+
+def prime_factors(integer)
+	array_primos = numeros_primos(integer)
+	new_array = []
+	for m in array_primos
+		# p "iteracion #{m} #{array_primos}"
+		if integer % m == 0
+			new_array.push(m)
+			nv = integer / m
+			if nv % m == 0
+				new_array.push(m)
+			end
+		end
+		# p "resultado   #{array_primos}"
+		# p "new_array   #{new_array}"
+	end
+		new_array
 end
 
-puts numeros_primos(22)
-# p prime_factors(9) == [3, 3]
-# p prime_factors(12) == [2, 2, 3]
-# p prime_factors(34) == [2, 17]
+p prime_factors(102)
+
+p prime_factors(9) == [3, 3]
+p prime_factors(12) == [2, 2, 3]
+p prime_factors(34) == [2, 17]
+p prime_factors(102) == [2,3, 17]
+p prime_factors(4568)
